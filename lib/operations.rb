1ea4669248fb8cc08274a52d078baa1f4e2a9d5c
james hall <halljames631@gmail.com>
2019-12-09 13:46:22 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,22 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return true 
+  elsif speed < 40
+    return true 
+  else 
+    return false 
+  end
 end
 
 
 
 def not_safe?(speed)
+  
+  return speed > 60 ? true : false
+	return speed < 60 ? true : false
+	return speed > 40 ? true : true
 	
+	binding.pry
 end
 	
 
