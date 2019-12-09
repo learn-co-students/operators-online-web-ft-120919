dd92dc4725cf3093b7dc10d94994feae9780772b
james hall <halljames631@gmail.com>
2019-12-09 14:00:34 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -15,7 +15,7 @@ end
 def not_safe?(speed)
   return speed > 60 || 40 ? true : false
   return speed < 60  ? true : false
-  return !speed <= 60 && !speed > 40 ? true : false
+  return speed < 60 && speed > 40 ? true : false
   
 end
 	
