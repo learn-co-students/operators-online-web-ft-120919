ae5787c2ce27c8e2df012235b36103255511a025
Ho Yin Keung <hykeung92@gmail.com>
2019-12-10 19:27:40 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,13 @@
 def unsafe?(speed)
-
+  return true if speed > 60
+  return true if speed < 40
+  return false if speed.between?(40,60)
 end
 
 
 
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ? true : false
 end
 	
 
