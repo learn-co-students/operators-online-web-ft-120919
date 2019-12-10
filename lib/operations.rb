eacd5f31bb6cf52cf02821e8c23ecb6f78a5a930
Tammy Lam <lamtammy1515@gmail.com>
2019-12-10 02:31:15 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,13 @@
 def unsafe?(speed)
-
+(Going 30 mph on the freeway would be unsafe, as would going 95 mph.).to eq(true)
+(Going 50 miles per hour, however, would return false as that's within the "safe" range.).to eq(true)
 end
 
 
 
 def not_safe?(speed)
-	
+	(Going 30 mph on the freeway would be not_safe, as would going 95 mph.).to eq(true)
+(Going 50 miles per hour, however, would return false as that's within the "not_safe" range.).to eq(true)
 end
 	
 
