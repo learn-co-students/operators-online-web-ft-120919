2b7a2239b46292c032b9edea69c96cde0324bd56
confident-token-1019 <theoonetj@gmail.com>
2019-12-14 06:27:54 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
 def unsafe?(speed)
 
+
+if spseed < 40 || spseed >60
+  return "Going 30 mph on the freeway would be unsafe, as would going 95 mph."
+else 
+  return "safe range"
+end
 end
 
 
 
 def not_safe?(speed)
+  speed < 40 || speed >60? "Going 30 mph on the freeway would be unsafe, as would going 95 mph" : "safe range"
 	
 end
 	
 
-
+speed(50)
