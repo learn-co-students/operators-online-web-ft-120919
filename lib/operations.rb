844322f9a4a160dbf72b48dbe031328a38ec951d
Mikael Kaya <kayaswork@gmail.com>
2019-12-19 19:39:44 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,14 @@
 def unsafe?(speed)
-
+  if "speed = <40 : >60"
+    return TRUE
+  else "speed = >40 : <60"
+    return FALSE
 end
 
 
 
 def not_safe?(speed)
-	
+	"speed = <40 : >60" ? "speed = >40 : <60" :
 end
 	
 
